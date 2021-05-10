216aaf6fc281e257865135138ce6bbf8a1ff134b
Maximus <mu5cool@yandex.ru>
2021-05-10 13:46:58 +0300

    singleton

---
+++ src/com/company/singleton/TestSingleton.java
@@ -0,0 +1,20 @@
+package com.company.singleton;
+
+public class TestSingleton {
+
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+
+    public void print() {
+        System.out.println(instance);
+    }
+}
